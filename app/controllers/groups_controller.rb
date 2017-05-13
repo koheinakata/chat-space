633ead6d851e3b869bdff131c0ec415697c75cfb
Kouhei Nakata <koheinakata0508@gmail.com>
2017-05-13 18:35:10 +0900

    グループを作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,30 @@
 class GroupsController < ApplicationController
-  def edit
-
+  def new
+    @group = Group.new
   end
 
-  def new
+  def create
+    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループ作成に成功しました"
+    else
+      flash.now[:alert] = "グループ作成に失敗しました"
+    end
+  end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 end
