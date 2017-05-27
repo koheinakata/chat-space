f2ae9167ecf56c0d04840c54417c8c697a1d83bb
Kouhei Nakata <koheinakata0508@gmail.com>
2017-05-27 21:33:18 +0900

    グループ機能「グループ追加機能」

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506134952) do
+ActiveRecord::Schema.define(version: 20170514024958) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",    null: false
@@ -56,4 +56,8 @@ ActiveRecord::Schema.define(version: 20170506134952) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
